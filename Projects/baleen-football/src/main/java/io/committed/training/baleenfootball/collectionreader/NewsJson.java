fba07a66141e7725c704d5e376f6517399287fba
Chris Flatley <chris@committed.io>
2018-07-21 22:19:19 +0200

    Add implementation for saveToJCas

--- Projects/baleen-football/src/main/java/io/committed/training/baleenfootball/collectionreader/NewsJson.java
+++ Projects/baleen-football/src/main/java/io/committed/training/baleenfootball/collectionreader/NewsJson.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.DocumentAnnotation;
 import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.committed.training.baleenfootball.utils.AbstractConfigurableFolderReader;
+import uk.gov.dstl.baleen.uima.UimaSupport;
 
 public class NewsJson extends AbstractConfigurableFolderReader {
 
@@ -47,9 +49,28 @@ public class NewsJson extends AbstractConfigurableFolderReader {
     return a;
   }
 
-  private void saveToJCas(JCas jCas, Article article) {
-    // TODO Auto-generated method stub
+  private void saveToJCas(JCas jCas, Article a) {
+    // Set the document text
+    jCas.setDocumentText(a.getContent());
 
+    // The document annotation is a special place that Baleen stores
+    // its own important per document metadata.
+
+    final DocumentAnnotation da = UimaSupport.getDocumentAnnotation(jCas);
+    da.setTimestamp(a.getBestTimestamp());
+    da.setDocType(a.getSourceType());
+    da.setDocumentClassification("O");
+    da.setSourceUri(a.getBestSource());
+
+    // Add some meta
+    // but sourceeType -> da.getDocType
+    // description -> summary
+
+    addMetadata(jCas, "summary", a.getSummary().orElse(null));
+    addMetadata(jCas, "title", a.getTitle());
+    addMetadata(jCas, "url", a.getUrl());
+    addMetadata(jCas, "sourceUrl", a.getSourceUrl());
+    addMetadata(jCas, "localFilename", a.getLocalFilename());
   }
 
 
